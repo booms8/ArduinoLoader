855f46241ef82c8c0d8048a90fafacbb996bd1b1
Conor Freeland <cfreeland@summa-tech.com>
2017-05-08 12:52:28 -0400

    Updated encoding

--- c_files/message.c
+++ c_files/message.c
@@ -1,4 +1,4 @@
 #include "chars.h"
 
-static int mode = 1;
-static byte pattern[11][8] = {one,two,three,four,five,six,seven,eight,nine,zero,space};
+static int mode = 0;
+static byte pattern[37][8] = {_A,_B,_C,_D,_E,_F,_G,_H,_I,_J,_K,_L,_M,_N,_O,_P,_Q,_R,_S,_T,_U,_V,_W,_X,_Y,_Z,one,two,three,four,five,six,seven,eight,nine,zero,space};
